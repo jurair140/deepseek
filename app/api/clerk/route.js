5fd427d0da73475676693bd7fc27d73f1869492a
jurair <jurair140@gmail.com>
2025-04-29 16:27:42 +0530

    'forth'

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -1,50 +1,43 @@
 import connectDB from "@/config/db";
 import User from "@/model/User";
 import { headers } from "next/headers";
-import {  NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-
-export async function POST(req){
-    const wh = new Webhook(process.env.SIGHNING_SECRET)
-    const headerPayload = await headers()
+export async function POST(req) {
+    const wh = new Webhook(process.env.SIGHNING_SECRET);
+    const headerPayload = headers(); // no need to await
     const svixHeaders = {
-        "svix-id" : headerPayload.get("svix-id"),
-        "svix-signature" : headerPayload.get("svix-signature"),
-    }
-
-    // get the payload and varify
+        "svix-id": headerPayload.get("svix-id"),
+        "svix-signature": headerPayload.get("svix-signature"),
+    };
 
     const payload = await req.json();
-    const body = JSON.stringify(payload)
-    const {data, type} = wh.verify(body, svixHeaders)
+    const body = JSON.stringify(payload);
+    const { data, type } = wh.verify(body, svixHeaders);
 
-    // prepare the user data to be saved inthe database
+    const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    };
 
-    const userData  = {
-        _id:data.id,
-        email:data.email_addresses[0].email_address,
-        name:`${data.first_name} ${data.last_name}`,
-        image:data.image_url
-    }
-    await connectDB()
+    await connectDB();
 
     switch (type) {
-        case 'user.created':
-            await User.create(userData)
+        case "user.created":
+            await User.create(userData);
             break;
-
-        case 'user.updated':
-            await User.findByIdAndUpdate(data.id, userData)
+        case "user.updated":
+            await User.findByIdAndUpdate(data.id, userData);
             break;
-        
-        case 'user.deleted':
-            await User.findByIdAndDelete(data.id)
-            break;       
-    
+        case "user.deleted":
+            await User.findByIdAndDelete(data.id);
+            break;
         default:
             break;
     }
 
-    return NextResponse.json({message:"Event recieved"})
+    return NextResponse.json({ message: "Event received" });
 }
