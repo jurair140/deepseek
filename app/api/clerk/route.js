e9d7517d7002b25eabc195c166f74d2f946e5e93
jurair <jurair140@gmail.com>
2025-04-29 16:01:36 +0530

    first-commit

---
+++ app/api/clerk/route.js
@@ -0,0 +1,49 @@
+import connectDB from "@/config/db";
+import User from "@/model/User";
+import { NextRequest } from "next/server";
+import { Webhook } from "svix";
+
+
+export async function POST(reg){
+    const wh = new Webhook(process.env.SIGHNING_SECRET)
+    const headerPayload = await headers()
+    const svixHeaders = {
+        "svix-id" : headerPayload.get("svix-id"),
+        "svix-signature" : headerPayload.get("svix-signature"),
+    }
+
+    // get the payload and varify
+
+    const payload = await requestAnimationFrame.json();
+    const body = JSON.stringify(payload)
+    const {data, type} = wh.verify(body, svixHeaders)
+
+    // prepare the user data to be saved inthe database
+
+    const userData  = {
+        _id:data.id,
+        email:data.email_addresses[0].email_address,
+        name:`${data.first_name} ${data.last_name}`,
+        image:data.image_url
+    }
+    await connectDB()
+
+    switch (type) {
+        case 'user.created':
+            await User.create(userData)
+            break;
+
+        case 'user.updated':
+            await User.findByIdAndUpdate(data.id)
+            break;
+        
+        case 'user.deleted':
+            await User.findByIdAndDelete(data.id)
+            break;       
+    
+        default:
+            break;
+    }
+
+    return NextRequest.json({message:"Event recieved"})
+}
